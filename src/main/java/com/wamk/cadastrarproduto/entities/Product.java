23f417024e5c84205132166ab5b7187764314385
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-07-05 13:10:07 -0300

    Conexão com o banco de dados Postgres e Entidade Produto

---
+++ src/main/java/com/wamk/cadastrarproduto/entities/Product.java
@@ -0,0 +1,73 @@
+package com.wamk.cadastrarproduto.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private String productName;
+	private Double value;
+	
+	public Product() {
+	}
+
+	public Product(UUID id, String productName, Double value) {
+		this.id = id;
+		this.productName = productName;
+		this.value = value;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ src/test/java/com/wamk/cadastrarproduto/CadastrarprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wamk.cadastrarproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrarprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
